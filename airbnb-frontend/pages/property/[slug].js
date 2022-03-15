9233a86e4f62dbb827f66f3e665fd808dc199616
Agacy junior <juniormartinssom@gmail.com>
2022-03-15 16:13:04 -0300

    Finish Front-end

--- airbnb-frontend/pages/property/[slug].js
+++ airbnb-frontend/pages/property/[slug].js
@@ -1,5 +1,9 @@
-import { sanityClient } from "../../sanity";
-import { isMultiple } from "../../utils";
+import { sanityClient } from "../../sanity"
+import { isMultiple } from "../../utils"
+import Image from "../../components/Image"
+import Review from "../../components/Review"
+import Map from "../../components/Map"
+import Link from "next/link"
 
 const Property = ({
   title,
@@ -14,32 +18,91 @@ const Property = ({
   host,
   reviews,
 }) => {
+  const reviewAmount = reviews.length
 
-  const reviewAmount = reviews.length;
   return (
     <div className="container">
-      <h1><b>{title}</b></h1>
-      <h2><b>{propertyType} hosted by {host?.name}</b></h2>
-      <h4>{bedrooms} bedroom{isMultiple(bedrooms)} * {beds} bed{isMultiple(beds)}</h4>
-      <hr />
-      <h4><b>Enhanced Clean</b></h4>
-      <p>This host is committed to Airbnbs 5-step enhanced cleaning process</p>
-      <h4><b>Amenities for everyday living</b></h4>
-      <p>The host has equipped this place for long stays</p>
-      <h4><b>House rules</b></h4>
-      <p>This place suitable for pets and the host does not allow</p>
-
-      <div className="price-box">
-        <h2>R${pricePerNight}</h2>
-        <h4>{reviewAmount} review{isMultiple(reviewAmount)}</h4>
-        <div className="button" onClick={() => {}}>Change Dates</div>
+      <h1>
+        <b>{title}</b>
+      </h1>
+      <p>
+        {reviewAmount} review{isMultiple(reviewAmount)}
+      </p>
+      <div className="images-section">
+        <Image identifier="main-image" image={mainImage} />
+        <div className="sub-images-section">
+          {images.map(({ _key, asset }, image) => (
+            <Image key={_key} identifier="image" image={asset} />
+          ))}
+        </div>
       </div>
+
+      <div className="section">
+        <div className="information">
+          <h2>
+            <b>
+              {propertyType} hosted by {host?.name}
+            </b>
+          </h2>
+          <h4>
+            {bedrooms} bedroom{isMultiple(bedrooms)} * {beds} bed
+            {isMultiple(beds)}
+          </h4>
+          <hr />
+          <h4>
+            <b>Enhanced Clean</b>
+          </h4>
+          <p>
+            This host is committed to Airbnbs 5-step enhanced cleaning process.
+          </p>
+          <h4>
+            <b>Amenities for everyday living</b>
+          </h4>
+          <p>
+            The host has equipped this place for long stays - kitchen, shampoo,
+            conditioner, hairdryer included.
+          </p>
+          <h4>
+            <b>House rules</b>
+          </h4>
+          <p>
+            This place isnt suitable for pets andthe host does not allow
+            parties or smoking.
+          </p>
+        </div>
+        <div className="price-box">
+          <h2>£{pricePerNight}</h2>
+          <h4>
+            {reviewAmount} review{isMultiple(reviewAmount)}
+          </h4>
+          <Link href="/">
+            <div className="button">Change Dates</div>
+          </Link>
+        </div>
+      </div>
+
+      <hr />
+
+      <h4>{description}</h4>
+
+      <hr />
+
+      <h2>
+        {reviewAmount} review{isMultiple(reviewAmount)}
+      </h2>
+      {reviewAmount > 0 &&
+        reviews.map((review) => <Review key={review._key} review={review} />)}
+
+      <hr />
+
+      <h2>Location</h2>
+      <Map location={location}></Map>
     </div>
-  );
-};
+  )
+}
 
 export const getServerSideProps = async (pageContext) => {
-  const pageSlug = pageContext.query.slug;
+  const pageSlug = pageContext.query.slug
 
   const query = `*[ _type == "property" && slug.current == $pageSlug][0]{
     title,
@@ -66,15 +129,15 @@ export const getServerSideProps = async (pageContext) => {
         image
       }
     }
-  }`;
+  }`
 
-  const property = await sanityClient.fetch(query, { pageSlug });
+  const property = await sanityClient.fetch(query, { pageSlug })
 
   if (!property) {
     return {
       props: null,
       notFound: true,
-    };
+    }
   } else {
     return {
       props: {
@@ -90,8 +153,8 @@ export const getServerSideProps = async (pageContext) => {
         host: property.host,
         reviews: property.reviews,
       },
-    };
+    }
   }
-};
+}
 
-export default Property;
+export default Property
